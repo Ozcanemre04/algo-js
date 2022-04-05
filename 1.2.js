6bd7dda00e08c4273a1a40ea874d92c827060401
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-05 14:49:57 +0200

    2.1 js fini

--- 1.2.js
+++ 1.2.js
@@ -3,3 +3,6 @@ let Name = "Ozcan ";
 let city = "bruxelles";
 
 console.log("your name is " + Name + firstName +" and you live in " + city);
+
+
+
---
+++ 2.1.js
@@ -0,0 +1,10 @@
+var x = prompt("enter your age?");
+
+if (x >= 18){
+    alert("you are an adult");
+}
+
+else{
+    alert("you are not yet an adult")
+}
+
