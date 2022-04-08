905367852f72ecf98ab233a0ae10fdd41eca2d35
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-08 15:28:15 +0200

    3.4js

--- 3.4.js
+++ 3.4.js
@@ -1,11 +1,13 @@
 console.log(3.4 + "JS");
-let xy = [100,52,300]
+let xy = [100,52,600,300]
 console.log(xy);
 //min and max element
+console.log("min and max");
 console.log(Math.min(...xy));
 console.log(Math.max(...xy));
 
 
 //first and last element
+console.log("first and last element");
 console.log(xy[0]);
 console.log(xy[xy.length-1]);
