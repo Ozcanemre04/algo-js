c4cdc05aabf9e8bf5fe4b7863e47cea0c91d5689
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-08 11:42:20 +0200

    3.1 and 3.2

--- 3.2.js
+++ 3.2.js
@@ -3,7 +3,7 @@ let sum = 0;
 
 
 for (let i = 0; i < y.length; i++) {
-    sum += y[i];
+     sum += y[i]/y.length;
 }
 
 console.log(sum);
@@ -16,7 +16,7 @@ var x = [100,101,102];
 let summ=0;
 
 for (let n = 0; n < x.length; n++) {
-    summ += x[n];
+    summ += x[n]/x.length;
 }
 
 console.log(summ);
