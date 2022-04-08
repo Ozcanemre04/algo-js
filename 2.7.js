161c1efc9abb1af7649113faa6d296b0eb785432
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-08 10:39:54 +0200

    2.7 and 3.0

--- 2.7.js
+++ 2.7.js
@@ -1,1 +1,16 @@
+var n= Number(prompt("n:"));
+let final =0;
+let ask=0;
+
+
+while(n!=0){
+    ask=Number(prompt("n:"))
+     final+=ask;
+    n--;
+}
+alert('the result is ' + final)
+
+
+
+
 
---
+++ 3.0.js
@@ -0,0 +1,21 @@
+var x = [
+  "Gilles Raepsaet",
+  "jamie",
+  "jason ponda vinda",
+  "leoJeremy",
+  "martin",
+  "ozcanemre",
+  "rayane",
+  "ricardo",
+  "steffanie",
+  "sebastien",
+  "tanguy-hartiel",
+  "thibaud",
+  "zakaria",
+];
+
+
+for(let elem of x){
+    console.log(elem);
+}
+
