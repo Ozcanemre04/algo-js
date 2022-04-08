03733c29a77e8f71df3a7eee67f5545291c25b2a
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-08 16:34:01 +0200

    modif du 3.3js

--- 3.3.js
+++ 3.3.js
@@ -1,9 +1,8 @@
-
 //1
 
-let array = [10,20,30,40];
+let array = [10, 20, 30, 40];
 let arrayCopy = [...array];
-arrayCopy.push(6)
+
 console.log(array);
 console.log(arrayCopy);
 
@@ -16,20 +15,29 @@ console.log(arrayCopy);
 
 //other method
 
-let arr = [1,2,3,4,5];
+let arr = [1, 2, 3, 4, 5];
 let copy = arr.slice();
-copy.push(6)
- console.log(arr);
- console.log(copy);
+
+console.log(arr);
+console.log(copy);
 
 
- //push method
+//push method
 
- let arr2 =[2,4,6,8];
- let copy2 = [];
- copy2.push(...arr2);
- copy2.push(10)
+let arr2 = [2, 4, 6, 8];
+let copy2 = [];
+copy2.push(...arr2);
 
- console.log(arr2);
- console.log(copy2);
 
+console.log(arr2);
+console.log(copy2);
+
+// copy with concat
+console.log("concat");
+
+let arr3 = [10, 20, 30, 40];
+
+let copy3 = arr3.concat()
+
+console.log(arr3);
+console.log(copy3);
